e55ae3401bb5dfef905effb4674dc7ea95c70903
Minal Karani <karani.minal@gmail.com>
2018-12-12 14:55:48 -0500

    Giphy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,33 @@
-//Question Answers Object
+//Animals Array
 Topics=[
     "Dog",
     "Cat",
     "Rabit",
-    "Hamster"
+    "Hamster",
+    "Chicken",
+    "Frog",
+    "Turtle",
+    "Skunk",
+    "Bird"
 ];
 
 
 var intervalId;
-var time = 10;
-var timerRunning = false;
-var currentQuestion=0;
-var correctAnswers=0;var incorrectAnswers=0;var unanswered=0;
 var newText="";
-var i=0
+var i=0;
+var queryurl;
+
+//API Key
+var APIvalue="RIMwrYvXc0dkhKYzON01zcvaeucMiCDC";
+
+var baseurl = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&limit=3&q="
 
 
-
-//Major function 
+//Create Buttons with Animal Names  
 function CreateBtn(){
     $('.buttons').empty();
     $('.btns').empty();
+    
         for(i=0;i<Topics.length;i++)
         {
             var Btn = $("<button>");
@@ -31,20 +38,42 @@ function CreateBtn(){
             console.log(Topics.length);
             $('.buttons').append(Btn);
         }
-
+    
+    
 }
-//CreateBtn();
+
+//function to load Gif (from Giphy) on click of Animal button 
+function loadGif()
+    {
+        $.ajax({
+            url: queryurl,
+            method: 'GET'
+          }).done(function(result) {
+            console.log(result);
+            $("#gif").prepend("<img src="+result.data.images.downsized_still.url+">");
+        });
+    }
+
+CreateBtn();
 $(".submitBtn").on("click",function()
 {
  
     newText=$("#animal").val().trim();
+    
+    // Create button only if no input or input already in Topics Array
+    if((newText!=="")&&(!(Topics.includes(newText))))
+    {
     Topics.push(newText);
-    //alert(Topics);
     CreateBtn();
-   
+    }
+    else
+    {
+    return false;
+    }
     console.log(newText);
     console.log(Topics);
     return false;
+    
 });
 
 $(".btns").on("click",function()
@@ -54,6 +83,9 @@ $(".btns").on("click",function()
     btnId=$(this).attr("id");
     console.log(btnId);
     btnVal=$(this).text();
+    queryurl=baseurl+btnVal;
+    console.log(queryurl);
+    loadGif();
     console.log(btnVal);
     
 })
